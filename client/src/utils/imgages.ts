97da4b12c579b4211b94b56670f2ef692379deba
Niten Solanki <nitensolanki02@gmail.com>
2021-11-18 01:28:29 +0530

    Feat: Product card design

---
+++ client/src/utils/imgages.ts
@@ -0,0 +1,14 @@
+const images = [
+  {
+    id: "bussiness_man",
+    name: "Bussiness man",
+    src: "https://tailwindcss.com/_next/static/media/fancy-suit-jacket.380b02296e758f6a462590ecfb936789.jpg",
+  },
+  {
+    id: "midern_girl",
+    name: "Modern girl",
+    src: "https://tailwindcss.com/_next/static/media/classic-utility-jacket.0f108046e151c8576017eaf383406fe6.jpg",
+  },
+];
+
+export default images;
